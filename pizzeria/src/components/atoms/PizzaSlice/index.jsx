f4441fd4f29f738214e2929d989f8dbc2cc0c01d
jakubwelninski <welniak18@gmail.com>
2022-03-03 20:43:54 +0100

    03.03.2022/3

--- pizzeria/src/components/atoms/PizzaSlice/index.jsx
+++ pizzeria/src/components/atoms/PizzaSlice/index.jsx
@@ -24,7 +24,7 @@ const PizzaSlice = ({imageType}) => {
         }
     },[])
     return (
-        <img src={imagePath} alt="Pizza Slice" />
+        <img src={imagePath} alt="Pizza Slice" className="pizzaSlice"/>
     );
 }
 
--- pizzeria/src/pages/Home/index.jsx
+++ pizzeria/src/pages/Home/index.jsx
@@ -3,11 +3,15 @@ import PizzaSlice from "../../components/atoms/PizzaSlice";
 import "./index.css";
 
 const Home = () => {
-    return (<div className="homeWrapper">
-        <PizzaSlice imageType={1} />
-        <PizzaSlice imageType={2} />
-        <PizzaSlice imageType={3} />
-    </div>);  
+    return (
+        <div className="homeWrapper">
+            <div className="pizzaSlices">
+                <PizzaSlice imageType={1} />
+                <PizzaSlice imageType={2} />
+                <PizzaSlice imageType={3} />
+            </div>
+        </div>
+    );  
 }
 
 export default Home;
