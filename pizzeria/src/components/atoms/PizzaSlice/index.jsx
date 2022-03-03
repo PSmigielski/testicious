96a8be96ea57122ce8c1e50b259c8b92ebebf855
psmigielski <walek.smigielski@gmail.com>
2022-03-03 19:53:56 +0100

    add PizzaSlice conditional rendering

--- pizzeria/src/components/atoms/PizzaSlice/index.jsx
+++ pizzeria/src/components/atoms/PizzaSlice/index.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
+import img1 from "../../../assets/pizza_slice_1.svg"
+import img2 from "../../../assets/pizza_slice_2.svg"
+import img3 from "../../../assets/pizza_slice_3.svg"
 
-const PizzaSlice = ({imagePath}) => {
+
+const PizzaSlice = ({imageType}) => {
+    const [imagePath, setImagePath] = useState(""); 
+    useEffect(()=>{
+        switch(imageType){
+            case 1:
+                setImagePath(img1);
+                break;
+            case 2:
+                setImagePath(img2);
+                break;
+            case 3:
+                setImagePath(img3);
+                break;
+            default:
+                setImagePath(img1);
+                break;
+        }
+    },[])
     return (
         <img src={imagePath} alt="Pizza Slice" />
     );
--- pizzeria/src/pages/Home/index.jsx
+++ pizzeria/src/pages/Home/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import PizzaSlice from "../../components/atoms/PizzaSlice";
 import "./index.css";
 
 const Home = () => {
     return (<div className="homeWrapper">
-        <PizzaSlice />
+        <PizzaSlice imageType={1} />
+        <PizzaSlice imageType={2} />
+        <PizzaSlice imageType={3} />
     </div>);  
 }
 
