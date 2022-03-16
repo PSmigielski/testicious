43f1d70ac85882a096ffa5d3b47f05fe34d6bc85
jakubwelninski <welniak18@gmail.com>
2022-03-16 22:32:52 +0100

    16.03.2022/1

--- pizzeria/src/components/atoms/Particle/index.jsx
+++ pizzeria/src/components/atoms/Particle/index.jsx
@@ -1,8 +1,26 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./index.css";
 
-const Particle = ({image, top = undefined, left = undefined, right = undefined, bottom = undefined}) => {
-    const [style, setStyle] = useState({ top, left, right, bottom});
+const Particle = ({image, top = undefined, right = undefined}) => {
+    let ox = 40;
+    let oy = 20;
+
+   const [style, setStyle] = useState({top, right});
+
+    const sliding = (x,y,xdif, ydif) =>{
+        x+=xdif;
+        y+=ydif;
+        console.log(x,y)
+    }
+    setInterval(sliding(ox,oy,1,1),100);
+
+    useEffect(() => {
+        setInterval(() => {
+          console.log(ox)
+          ox++;
+        }, 1000);
+      }, []);
+
     return (<img src={image} alt="particle" style={style} className="particle"/>)
 };
 
--- pizzeria/src/pages/Home/index.jsx
+++ pizzeria/src/pages/Home/index.jsx
@@ -5,14 +5,15 @@ import Hamburger from "../../components/atoms/Hamburger";
 import Particle from "../../components/atoms/Particle";
 import "./index.css";
 import icon1 from "../../assets/mini_icon_1.svg"
-import icon2 from "../../assets/mini_icon_2.svg"
-import icon3 from "../../assets/mini_icon_3.svg"
-import icon4 from "../../assets/mini_icon_4.svg"
-import icon5 from "../../assets/mini_icon_5.svg"
-import icon6 from "../../assets/mini_icon_6.svg"
-import icon7 from "../../assets/mini_icon_7.svg"
+// import icon2 from "../../assets/mini_icon_2.svg"
+// import icon3 from "../../assets/mini_icon_3.svg"
+// import icon4 from "../../assets/mini_icon_4.svg"
+// import icon5 from "../../assets/mini_icon_5.svg"
+// import icon6 from "../../assets/mini_icon_6.svg"
+// import icon7 from "../../assets/mini_icon_7.svg"
 
 const Home = () => {
+    
 
     return (
         <div className="homeWrapper">
@@ -22,13 +23,13 @@ const Home = () => {
                 <PizzaSlice imageType={3} />
             </div>
             <div className="particles">
-                <Particle image={icon1} top={"20%"}/>
-                <Particle image={icon2} />
+                <Particle image={icon1} />
+                {/* <Particle image={icon2} />
                 <Particle image={icon3} />
                 <Particle image={icon4} />
                 <Particle image={icon5} />
                 <Particle image={icon6} />
-                <Particle image={icon7} />
+                <Particle image={icon7} /> */}
             </div>
             <PepperPic />
             <Hamburger />
