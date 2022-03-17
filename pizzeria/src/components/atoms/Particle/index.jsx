7f90a4939fd520ba5d6dae8e4ac1f886ceac236c
psmigielski <walek.smigielski@gmail.com>
2022-03-17 21:10:34 +0100

    fix refs

--- pizzeria/src/components/atoms/Particle/index.jsx
+++ pizzeria/src/components/atoms/Particle/index.jsx
@@ -1,11 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./index.css";
 
-const Particle = ({image, top = undefined, right = undefined}) => {
+const Particle = React.forwardRef(({image, top = undefined, right = undefined}, ref) => {
 
     const [style, setStyle] = useState({top, right});
 
-    return (<img src={image} alt="particle" style={style} className="particle"/>)
-};
+    return (<img src={image} alt="particle" style={style} className="particle" ref={ref}/>)
+});
 
 export default Particle;
