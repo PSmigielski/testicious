57815521fcc50fea0c6f8407d0b0496890897197
KrupaMateusz <krupamateusz@onet.pl>
2022-03-22 09:59:59 +0100

    Dodane do kontaktu co jest źle ale poprawimy, menu

--- pizzeria/src/Router.jsx
+++ pizzeria/src/Router.jsx
@@ -4,6 +4,7 @@ import './main.css';
 import AppShell from "./pages/AppShell";
 import Home from "./pages/Home";
 import MenuNav from "./pages/Menu";
+import Contact from "./pages/Contact";
 import PizzaOfTheDay from "./pages/PizzaOfTheDay";
 const Router = () => {
 
@@ -13,6 +14,7 @@ const Router = () => {
       <Route path="/" element={<AppShell />}>
         <Route index element={<Home />}/>
         <Route path="/menu" element={<MenuNav />}/>
+        <Route path="/contact" element={<Contact />}/>
         <Route path="/pizza-of-the-day" element={<PizzaOfTheDay />}/>
       </Route>
       <Route path="*" element={<div>404</div>}/>
