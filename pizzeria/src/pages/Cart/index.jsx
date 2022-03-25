f7b5d2c685a53f621d5180be11393ef215283726
psmigielski <walek.smigielski@gmail.com>
2022-03-25 20:51:24 +0100

    add empty cart message

--- pizzeria/src/pages/Cart/index.jsx
+++ pizzeria/src/pages/Cart/index.jsx
@@ -1,14 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartItem from "../../components/atoms/CartItem";
 import { CartContext } from "../../contexts/CartContext";
 import "./index.css"
 
 const Cart = () => {
     const {items} = useContext(CartContext);
+    const [hasItems, setHasItems] = useState(false);
+    useEffect(()=>{
+        if(items){
+            setHasItems(true);
+        }else{
+            setHasItems(false);
+        }
+    },[items])
     return (
-    <div className="cartWrapper"> 
+    <div className="cartWrapper" style={!hasItems ? {justifyContent : "center"} : {justifyContent : "flex-start"}}> 
         <div className="cartItems">
-            {items.map(el => (<CartItem key={el.id} data={el}/>))}
+            {hasItems ? items.map(el => (<CartItem data={el}/>)) 
+            : (<div className="emptyCart"><p>Koszyk jest pusty</p></div>)}
         </div>
     </div>)
 }
