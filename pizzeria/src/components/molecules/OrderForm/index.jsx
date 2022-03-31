8c1107533f2b4d72501e270e5246b46b98813292
jakubwelninski <welniak18@gmail.com>
2022-03-31 23:00:58 +0200

    31.03.2022/3

--- pizzeria/src/components/molecules/OrderForm/index.jsx
+++ pizzeria/src/components/molecules/OrderForm/index.jsx
@@ -19,10 +19,19 @@ const OrderForm = ({isOpen, setIsOpen}) => {
         setName("");
         setIsOpen(false)
     }
+
+    const [paymnet,setPayment] = useState(false);
+    const handlePayment = () =>{
+        setPayment(true);
+        setStep(step+1);
+    }
+    const backgroundColor1 = "linear-gradient(223.14deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0.114583) 32.29%, rgba(221, 50, 30, 0.2) 97.46%, rgba(0, 0, 0, 0) 100%), #19222B;";
+    const backgroundColor1 = "linear-gradient(223.14deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0.114583) 32.29%, rgba(39, 153, 29, 0.2) 97.46%, rgba(0, 0, 0, 0) 100%), #19222B;";
+    
     if (!isOpen) return null;
     return ReactDOM.createPortal(
     <div className="orderFormWrapper">
-        <div className="orderFormBody">
+        <div className="orderFormBody" style={step !== 4 ? {backgroundColor: "linear-gradient(223.14deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0.114583) 32.29%, rgba(221, 50, 30, 0.2) 97.46%, rgba(0, 0, 0, 0) 100%), #19222B;"}}>
             <div className="orderFormHeader">
                 <div className="twoCol">
                     Złóż zamówienie
@@ -34,7 +43,7 @@ const OrderForm = ({isOpen, setIsOpen}) => {
             <div className="orderFormSteps">
                 <div className="orderFormStep" style={step>=1? {color: "red"}:{color: "white"}}>Dane</div>
                 <div className="orderFormStep" style={step>=2? {color: "red"}:{color: "white"}}>Adres</div>
-                <div className="orderFormStep" style={step==3? {color: "red"}:{color: "white"}}>Płatność</div>
+                <div className="orderFormStep" style={step>=3? {color: "red"}:{color: "white"}}>Płatność</div>
             </div>
             <div className="orderForm"> 
                 {step === 1 && <div className="bottomCol1">
@@ -94,9 +103,16 @@ const OrderForm = ({isOpen, setIsOpen}) => {
                     />                   
                 </div>}
                 {step === 3 && <div className="bottomCol1">
-                    <button className="paymentButton">Opłać</button>
+                    <button className="paymentButton" onClick={()=>handlePayment()}>Opłać</button>
                 </div>}
-                <div className="bottomCol2">
+
+                {step === 4 && paymnet &&<div className="bottomCol1">
+                    <p>Wszystko gotowe!</p>
+                
+                </div>}
+
+
+                {step !==4 && <div className="bottomCol2">
                     <button 
                         className="orderFormButton" 
                         onClick={() => setStep(step-1)} 
@@ -105,7 +121,7 @@ const OrderForm = ({isOpen, setIsOpen}) => {
                         className="orderFormButton" 
                         onClick={() => setStep(step+1)} 
                         style={step === 3 ? {visibility: "hidden", pointerEvents: "none"}: {visibility: "visible", pointerEvents: "all"}}>Dalej</button>
-                </div>
+                </div>}
             </div>
         </div>
     </div>, document.body)
