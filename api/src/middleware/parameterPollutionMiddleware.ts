1c14d6402ca2011a2c4e867c09bebc3d28bf8c3c
psmigielski <walek.smigielski@gmail.com>
2022-04-20 15:50:54 +0200

    remove console.logs

--- api/src/middleware/parameterPollutionMiddleware.ts
+++ api/src/middleware/parameterPollutionMiddleware.ts
@@ -4,15 +4,12 @@ import ApiErrorException from "../exceptions/ApiErrorException";
 const parameterPollutionMiddleware = (parameters: string | string[]) => {
     return (req: Request, res: Response, next: NextFunction) => {
         if (Array.isArray(parameters)) {
-            console.log("1");
             for (const parameter of parameters) {
-                console.log(parameter);
                 if (Array.isArray(req.query[parameter])) {
                     throw new ApiErrorException("You cannot specify two or more of the same parameter", 409);
                 }
             }
         } else {
-            console.log("2");
             if (Array.isArray(req.query[parameters])) {
                 throw new ApiErrorException("You cannot specify two or more of the same parameter", 409);
             }
