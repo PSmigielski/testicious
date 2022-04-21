6bbd0e8ce941e5683a54d1071af21fc3816e6990
jakubwelninski <welniak18@gmail.com>
2022-04-21 23:26:40 +0200

    21.04.2022/4 | add some stuff to home page

--- app/src/pages/Home/index.jsx
+++ app/src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-
+import HeroImage from "../../assets/HeroImage.svg"
 import "./index.scss";
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className="hero__container">
+            <p className="hero__paragraph">THE BEST <span className="hero__paragraph--marked">FOOD</span> FOR YOUR TASTE</p>
+            <p className="hero__paragraph__slave">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <img className="hero__img" src={HeroImage} alt="hero image" />
         </div>
     );
 };
