1a5d2ff57a64a6661353abce9e2906e8795215d4
psmigielski <walek.smigielski@gmail.com>
2022-04-21 21:48:05 +0200

    fix nav

--- app/src/pages/Home/index.jsx
+++ app/src/pages/Home/index.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import Logo from "../../components/atoms/Logo";
 import CartButton from "../../components/atoms/Cart";
-import Navigation from "../../components/organisms/Navigation";
-import "./index.scss"
+import Navigation from "../../components/molecules/Navigation";
+import "./index.scss";
 
 const Home = () => {
-
     return (
         <div>
             <div className="logo__container">
                 <Logo />
                 <CartButton />
-
             </div>
-            <div className="navigation__container">
-                <Navigation />
-
-            </div>
+            <Navigation />
         </div>
     );
-}
+};
 export default Home;
