c4ecc20f9d2c75e4bbd3a1002a2471987d115c3a
jakubwelninski <welniak18@gmail.com>
2022-04-22 15:30:19 +0200

    22.04.2022/4 | fix paragraph position

--- app/src/pages/Home/index.jsx
+++ app/src/pages/Home/index.jsx
@@ -5,8 +5,10 @@ import "./index.scss";
 const Home = () => {
     return (
         <div className="hero__container">
-            <p className="hero__paragraph">THE BEST <span className="hero__paragraph--marked">FOOD</span> FOR YOUR TASTE</p>
-            <p className="hero__paragraph__slave">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div className="hero__paragraph__container">
+                <p className="hero__paragraph">THE BEST <span className="hero__paragraph--marked">FOOD</span> FOR YOUR TASTE</p>
+                <p className="hero__paragraph__slave">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
             <img className="hero__img" src={HeroImage} alt="hero image" />
         </div>
     );
