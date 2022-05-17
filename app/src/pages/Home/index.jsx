a3625f60af3a52a2cb22ba19c9953b7e433e8a97
jakubwelninski <welniak18@gmail.com>
2022-05-17 13:24:37 +0200

    17.05.2022/1 fix positioning

--- app/src/pages/Home/index.jsx
+++ app/src/pages/Home/index.jsx
@@ -9,8 +9,8 @@ const Home = () => {
             <div className="hero__paragraph__container">
                 <p className="hero__paragraph">THE BEST <span className="hero__paragraph--marked">FOOD</span> FOR YOUR TASTE</p>
                 <p className="hero__paragraph__slave">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <img className="hero__img" src={HeroImage} alt="hero image" />
             </div>
+                <img className="hero__img" src={HeroImage} alt="hero image" />
         </section>
             
     );
