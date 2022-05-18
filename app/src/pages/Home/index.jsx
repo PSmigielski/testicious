cdd4f5262691ef6d9afb597575bed69d3dbbee54
jakubwelninski <welniak18@gmail.com>
2022-05-18 13:00:07 +0200

    18.05.2022/1 fix positioning

--- app/src/pages/Home/index.jsx
+++ app/src/pages/Home/index.jsx
@@ -10,15 +10,18 @@ const Home = () => {
                 <p className="hero__paragraph">THE BEST <span className="hero__paragraph--marked">FOOD</span> FOR YOUR TASTE</p>
                 <p className="hero__paragraph__slave">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
-                <img className="hero__img" src={HeroImage} alt="hero image" />
-                <div class="indicator">
-                    <div class="indicator__ball">
-                        <div class="indicator__ball__pointer"></div>
-                    </div>
-                </div>
+                <img className="hero__img" src={HeroImage} alt="hero image" />     
         </section>
             
     );
 };
 export default Home;
 
+{/* <div className="hero__indicator">
+<div class="indicator">
+        <div class="indicator__ball">
+            <div class="indicator__ball__pointer"></div>
+        </div>
+</div>
+</div>
+ */}
