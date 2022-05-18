32351237f60fab0bf52be96a7bd83984c806e693
jakubwelninski <welniak18@gmail.com>
2022-05-18 21:58:14 +0200

    18.05.2022/2 styling

---
+++ app/src/components/atoms/Indicator/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.scss";
+
+
+const Indicator = () => {
+    return (
+        <div class="indicator">
+            <div class="indicator__ball">
+                <div class="indicator__ball__pointer"></div>
+            </div>
+        </div>
+    );
+}; 
+export default Indicator;
+
+
--- app/src/pages/Home/index.jsx
+++ app/src/pages/Home/index.jsx
@@ -1,27 +1,26 @@
 import React from "react";
-import HeroImage from "../../assets/HeroImage.svg"
+import HeroImage from "../../assets/HeroImage.svg";
+import WaveImage from "../../assets/WaveImage.svg";
+import Indicator from "../../components/atoms/Indicator";
 import "./index.scss";
-import Hamburger from "../../components/atoms/Hamburger";
+
 
 const Home = () => {
     return (
-        <section className="hero__container">
-            <div className="hero__paragraph__container">
-                <p className="hero__paragraph">THE BEST <span className="hero__paragraph--marked">FOOD</span> FOR YOUR TASTE</p>
-                <p className="hero__paragraph__slave">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <section className="home">
+            <section className="hero__container">
+                <div className="hero__paragraph__container">
+                    <p className="hero__paragraph">THE BEST <span className="hero__paragraph--marked">FOOD</span> FOR YOUR TASTE</p>
+                    <p className="hero__paragraph__slave">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                    <img className="hero__img" src={HeroImage} alt="hero image" /> 
+            </section>
+            <div className="indicator__container">
+                <Indicator />
             </div>
-                <img className="hero__img" src={HeroImage} alt="hero image" />     
+            <img className="wave__img" src={WaveImage} alt="wave image" />
         </section>
             
     );
 };
 export default Home;
-
-{/* <div className="hero__indicator">
-<div class="indicator">
-        <div class="indicator__ball">
-            <div class="indicator__ball__pointer"></div>
-        </div>
-</div>
-</div>
- */}
