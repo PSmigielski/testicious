f7b8996d7f86467f23cb8ba495203f0005e5a0eb
jakubwelninski <welniak18@gmail.com>
2022-07-21 14:11:37 +0200

    fix smoe bugs

--- app/src/components/atoms/PreviewBlocks/PreviewLeftBlock/index.jsx
+++ app/src/components/atoms/PreviewBlocks/PreviewLeftBlock/index.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import PizzaImage2 from "../../../../assets/PizzaImage2.svg"
 import "./index.scss";
 
 const PreviewLeftBlock = ({img,name}) => {
+    
+    img = PizzaImage2;
+    name = "Capriciosa";
+    
     return (
         <div className="preview__leftblock">
-            <img className="preview__leftblock__img" src={img} alt="" />
+            <div className="preview__leftblock__imgblock" />
+                <img className="preview__leftblock__img" src={img} alt="" />
             <div className="preview__leftblock__name">
                 {name}
             </div>
--- app/src/components/atoms/PreviewBlocks/PreviewRightBlock/index.jsx
+++ app/src/components/atoms/PreviewBlocks/PreviewRightBlock/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import PizzaImage3 from "../../../../assets/PizzaImage3.svg"
 import "./index.scss";
 
 const PreviewRightBlock = ({img,name}) => {
-    return (
+
+    img = PizzaImage3;
+    name = "Quattro Stagioni";
+
+        return (
         <div className="preview__rightblock">
-            <img className="preview__rightblock__img" src={img} alt="" />
+            <div className="previev__rightblock__imgblock">
+                <img className="preview__rightblock__img" src={img} alt="" />
+            </div>
             <div className="preview__rightblock__name">
                 {name}
             </div>
