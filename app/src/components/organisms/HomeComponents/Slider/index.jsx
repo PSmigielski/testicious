a08f2e41cc6fbfee885cd9c594c207c8da57a852
jakubwelninski <welniak18@gmail.com>
2022-07-21 21:19:39 +0200

    add button animotion

--- app/src/components/organisms/HomeComponents/Slider/index.jsx
+++ app/src/components/organisms/HomeComponents/Slider/index.jsx
@@ -2,21 +2,26 @@ import React from "react";
 import PreviewLeftBlock from "../../../atoms/PreviewBlocks/PreviewLeftBlock";
 import PreviewRightBlock from "../../../atoms/PreviewBlocks/PreviewRightBlock";
 import PreviewCenterBlock from "../../../atoms/PreviewBlocks/PreviewCenterBlock";
+import ArrowButton from "../../../../assets/ArrowButton.svg"
 import "./index.scss";
 
 
 const Slider = () => {
     return (
         <section className="slider">
-            {/* <div className="slider__paragraph">
-                Lorem ipsum <span className="slider__paragraph--marked">dolor </span> 
-                sit amet, consectetur adipiscing elit
-            </div> */}
-            <div className="previev">
+            <div className="slider__preview">
+                <button className="slider__preview__button--left">
+                    <img src={ArrowButton} alt="" />
+                </button>
                 <PreviewLeftBlock />
-                <PreviewRightBlock />
                 <PreviewCenterBlock />
+                <PreviewRightBlock />
+                <button className="slider__preview__button--right">
+                    <img src={ArrowButton} alt="" />
+                </button>
             </div>
+            
+            
         </section>      
     );
 };
